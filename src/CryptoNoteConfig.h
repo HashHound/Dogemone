88c6df993e3a7df2893b9807cf5fcd8455ca6222
RealEther <aravindkishore21@gmail.com>
2024-07-17 09:34:11 +0530

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -24,6 +24,9 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
 
 namespace CryptoNote {
 namespace parameters {
